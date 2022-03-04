fec23d6b6e1f56c088496d52cd8fa12f47b46645
Pier-Luc Gendreau <pierluc@outlook.com>
2022-03-04 06:53:23 -0500

    Enable font optimization

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"
+            rel="icon"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&amp;display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,11 @@ import { Layout } from "../components/Layout";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Pier-Luc · Full-Stack Developer</title>
-        <link
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"
-          rel="icon"
-        />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
       </Head>
       <Layout />
-    </div>
+    </>
   );
 }
