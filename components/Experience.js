ef87fd3aa356baf1e837a3657fe3b7c46e5a38f9
Pier-Luc Gendreau <pierluc@outlook.com>
2022-05-03 21:15:04 -0400

    Split layout sections

---
+++ components/Experience.js
@@ -0,0 +1,192 @@
+export function Experience() {
+  return (
+    <>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 id="experience" className="text-2xl font-semibold text-gray-900">
+          Experience
+        </h1>
+      </div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+        <div className="py-4">
+          <div className="bg-white shadow overflow-hidden sm:rounded-lg mb-8">
+            <div className="px-4 py-5 border-b border-gray-200 sm:px-6">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                FOCUS Smart Fleet and Fuel Optimization
+              </h3>
+              <p className="mt-1 text-sm leading-5 text-gray-500">
+                Primarily working on a large web app with a TypeScript backend
+                and jQuery frontend
+              </p>
+            </div>
+            <div>
+              <dl>
+                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm leading-5 font-medium text-gray-500">
+                    Title
+                  </dt>
+                  <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                    Programmer
+                  </dd>
+                </div>
+                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm leading-5 font-medium text-gray-500">
+                    When
+                  </dt>
+                  <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                    March 2020 - Present
+                  </dd>
+                </div>
+                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm leading-5 font-medium text-gray-500">
+                    Responsibilities
+                  </dt>
+                  <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                    <ul className="list-disc list-inside">
+                      <li>
+                        Replaced in-house client build with webpack while
+                        retaining full backward compatibility with existing
+                        infrastructure
+                      </li>
+                      <li>
+                        Introduced the team to React and immediately saw
+                        significant gains in both quality and speed compared to
+                        existing code based on jQuery
+                      </li>
+                      <li>
+                        Quickly shipped a messaging component as part of a
+                        larger project
+                      </li>
+                      <li>
+                        Wrapping up our first project fully built on React, with
+                        the front-end&apos;s first integration tests
+                      </li>
+                      <li>
+                        Building mock-ups for an upcoming project, and
+                        integrating Storybook
+                      </li>
+                    </ul>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+          <div className="bg-white shadow overflow-hidden sm:rounded-lg mb-8">
+            <div className="px-4 py-5 border-b border-gray-200 sm:px-6">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                Classcraft
+              </h3>
+              <p className="mt-1 text-sm leading-5 text-gray-500">
+                Contributed to and maintained a large Meteor application built
+                with Node.js, React, Redux, Flow, Stylus, ElasticSearch, and
+                MongoDB
+              </p>
+            </div>
+            <div>
+              <dl>
+                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm leading-5 font-medium text-gray-500">
+                    Title
+                  </dt>
+                  <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                    Senior Developer
+                  </dd>
+                </div>
+                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm leading-5 font-medium text-gray-500">
+                    When
+                  </dt>
+                  <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                    August 2016 - March 2020
+                  </dd>
+                </div>
+                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm leading-5 font-medium text-gray-500">
+                    Responsibilities
+                  </dt>
+                  <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                    <ul className="list-disc list-inside">
+                      <li>
+                        Led a team of three other developers working on the core
+                        application
+                      </li>
+                      <li>
+                        Implemented Figma and Sketch designs using React and CSS
+                      </li>
+                      <li>
+                        Participated in validating the UX design of major
+                        features
+                      </li>
+                      <li>
+                        Triaged bugs and worked with the support team to solve
+                        customer facing issues
+                      </li>
+                      <li>
+                        Rebuilt authentication in a dedicated service and
+                        transferred knowledge to another team
+                      </li>
+                      <li>
+                        Automated and streamlined various workflows for internal
+                        teams
+                      </li>
+                    </ul>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+          <div className="bg-white shadow overflow-hidden sm:rounded-lg mb-8">
+            <div className="px-4 py-5 border-b border-gray-200 sm:px-6">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">
+                Sport Chrono
+              </h3>
+              <p className="mt-1 text-sm leading-5 text-gray-500">
+                Rebuilt the web app with Node.js, Angular.js, and MongoDB,
+                maintained desktop apps built with C# .NET and SQL Server, and
+                tinkered with RFID hardware
+              </p>
+            </div>
+            <div>
+              <dl>
+                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm leading-5 font-medium text-gray-500">
+                    Title
+                  </dt>
+                  <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                    Developer
+                  </dd>
+                </div>
+                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm leading-5 font-medium text-gray-500">
+                    When
+                  </dt>
+                  <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                    July 2013 - June 2016
+                  </dd>
+                </div>
+                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm leading-5 font-medium text-gray-500">
+                    Responsibilities
+                  </dt>
+                  <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                    <ul className="list-disc list-inside">
+                      <li>
+                        Migrated a manual, slow and complex procedure involving
+                        Microsoft Access to export results to delivering them
+                        automatically, in real-time
+                      </li>
+                      <li>
+                        Redesigned the web app both technically and visually
+                      </li>
+                      <li>Wrote extensive documentation</li>
+                      <li>Trained a dozen folks to use the timing system</li>
+                    </ul>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
